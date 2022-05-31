NetLogo/brazier

commit da8de529a38143286f79fa83f17aa52d601c570f

Refactor/Major: Switch from Browserify to ES6 modules

src/brazier/array.coffee
@@ -1,200 +1,196 @@
-{ eq                     } = require('./equals')
-{ maybe, None, Something } = require('./maybe')
-{ isArray                } = require('./type')
+import { eq                     } from './equals.js'
+import { maybe, None, Something } from './maybe.js'
+import { isArray                } from './type.js'
 
-arrayOps = {
+# forall t. (t -> Boolean) -> Array t -> Boolean
+all = (f) -> (arr) ->
+  for x in arr
+    if not f(x)
+      return false
+  true
 
-  # forall t. (t -> Boolean) -> Array t -> Boolean
-  all: (f) -> (arr) ->
-    for x in arr
-      if not f(x)
-        return false
-    true
+# forall t. Array t -> Array t -> Array t
+concat = (xs) -> (ys) ->
+  xs.concat(ys)
 
-  # forall t. Array t -> Array t -> Array t
-  concat: (xs) -> (ys) ->
-    xs.concat(ys)
+# forall t. t -> Array t -> Boolean
+contains = (x) -> (arr) ->
+  for item in arr
+    if eq(x)(item)
+      return true
+  false
 
-  # forall t. t -> Array t -> Boolean
-  contains: (x) -> (arr) ->
-    for item in arr
-      if eq(x)(item)
-        return true
-    false
+# forall t. (t -> String) -> Array t -> Object Number
+countBy = (f) -> (arr) ->
+  acc = {}
+  for x in arr
+    key      = f(x)
+    value    = acc[key] ? 0
+    acc[key] = value + 1
+  acc
 
-  # forall t. (t -> String) -> Array t -> Object Number
-  countBy: (f) -> (arr) ->
-    acc = {}
-    for x in arr
-      key      = f(x)
-      value    = acc[key] ? 0
-      acc[key] = value + 1
-    acc
+# forall t. Array t -> Array t -> Array t
+difference = (xs) -> (arr) ->
+  acc     = []
+  badBoys = unique(arr)
+  for x in xs
+    if not contains(x)(badBoys)
+      acc.push(x)
+  acc
 
-  # forall t. Array t -> Array t -> Array t
-  difference: (xs) -> (arr) ->
-    acc     = []
-    badBoys = arrayOps.unique(arr)
-    for x in xs
-      if not arrayOps.contains(x)(badBoys)
-        acc.push(x)
-    acc
+# forall t. (t -> Boolean) -> Array t -> Boolean
+exists = (f) -> (arr) ->
+  for x in arr
+    if f(x)
+      return true
+  false
 
-  # forall t. (t -> Boolean) -> Array t -> Boolean
-  exists: (f) -> (arr) ->
-    for x in arr
-      if f(x)
-        return true
-    false
+# forall t. (t -> Boolean) -> Array t -> Array t
+filter = (f) -> (arr) ->
+  for x in arr when f(x)
+    x
 
-  # forall t. (t -> Boolean) -> Array t -> Array t
-  filter: (f) -> (arr) ->
-    for x in arr when f(x)
-      x
+# forall t. (t -> Boolean) -> Array t -> Maybe t
+find = (f) -> (arr) ->
+  for x in arr
+    if f(x)
+      return Something(x)
+  None
 
-  # forall t. (t -> Boolean) -> Array t -> Maybe t
-  find: (f) -> (arr) ->
-    for x in arr
-      if f(x)
-        return Something(x)
-    None
+# forall t. (t -> Boolean) -> Array t -> Maybe Number
+findIndex = (f) -> (arr) ->
+  for x, i in arr
+    if f(x)
+      return Something(i)
+  None
 
-  # forall t. (t -> Boolean) -> Array t -> Maybe Number
-  findIndex: (f) -> (arr) ->
-    for x, i in arr
-      if f(x)
-        return Something(i)
-    None
-
-  # forall t u. (t -> Array u) -> Array t -> Array u
-  flatMap: (f) -> (arr) ->
-    arrs =
-      for x in arr
-        f(x)
-    [].concat(arrs...)
-
-  # forall t u. Array t -> Array u
-  flattenDeep: (arr) ->
-    acc = []
-    for x in arr
-      if isArray(x)
-        acc = acc.concat(arrayOps.flattenDeep(x))
-      else
-        acc.push(x)
-    acc
-
-  # forall t u. (u,t -> u) -> u -> Array t -> u
-  foldl: (f) -> (acc) -> (arr) ->
-    out = acc
-    for x in arr
-      out = f(out, x)
-    out
-
-  # forall t. (t -> ()) -> Array t -> ()
-  forEach: (f) -> (arr) ->
+# forall t u. (t -> Array u) -> Array t -> Array u
+flatMap = (f) -> (arr) ->
+  arrs =
     for x in arr
       f(x)
-    return
+  [].concat(arrs...)
 
-  # forall t. Array t -> Maybe t
-  head: (arr) ->
-    arrayOps.item(0)(arr)
+# forall t u. Array t -> Array u
+flattenDeep = (arr) ->
+  acc = []
+  for x in arr
+    if isArray(x)
+      acc = acc.concat(flattenDeep(x))
+    else
+      acc.push(x)
+  acc
 
-  # forall t. Array t -> Boolean
-  isEmpty: (arr) ->
-    arr.length is 0
+# forall t u. (u,t -> u) -> u -> Array t -> u
+foldl = (f) -> (acc) -> (arr) ->
+  out = acc
+  for x in arr
+    out = f(out, x)
+  out
 
-  # forall t. Number -> Array t -> Maybe t
-  item: (index) -> (xs) ->
-    if 0 <= index < xs.length then Something(xs[index]) else None
+# forall t. (t -> ()) -> Array t -> ()
+forEach = (f) -> (arr) ->
+  for x in arr
+    f(x)
+  return
 
-  # forall t. Array t -> t
-  last: (arr) ->
-    arr[arr.length - 1]
+# forall t. Array t -> Maybe t
+head = (arr) ->
+  item(0)(arr)
 
-  # forall t. Array t -> Number
-  length: (arr) ->
-    arr.length
+# forall t. Array t -> Boolean
+isEmpty = (arr) ->
+  arr.length is 0
 
-  # forall t u. (t -> u) -> Array t -> Array u
-  map: (f) -> (arr) ->
-    for x in arr
-      f(x)
+# forall t. Number -> Array t -> Maybe t
+item = (index) -> (xs) ->
+  if 0 <= index < xs.length then Something(xs[index]) else None
 
-  # forall t. (t -> Number) -> Array t -> t
-  maxBy: (f) -> (arr) ->
-    maxX = undefined
-    maxY = -Infinity
-    for x in arr
-      y = f(x)
-      if y > maxY
-        maxX = x
-        maxY = y
-    maybe(maxX)
+# forall t. Array t -> t
+last = (arr) ->
+  arr[arr.length - 1]
 
-  # forall t. Array t -> Array t
-  reverse: (xs) ->
-    xs[..].reverse()
+# forall t. Array t -> Number
+length = (arr) ->
+  arr.length
 
-  # forall t. t -> Array t
-  singleton: (x) ->
-    [x]
+# forall t u. (t -> u) -> Array t -> Array u
+map = (f) -> (arr) ->
+  for x in arr
+    f(x)
 
-  # forall t. (t -> Comparable) -> Array t -> Array t
-  sortBy: (f) -> (arr) ->
-    g = (x, y) ->
-      fx = f(x)
-      fy = f(y)
-      if fx < fy then -1 else if fx > fy then 1 else 0
-    arr.slice(0).sort(g)
-
-  # forall t. (t -> Comparable) -> Array t -> t -> Number
-  # Assumes that `arr` is already sorted in terms of `f`
-  sortedIndexBy: (f) -> (arr) -> (x) ->
+# forall t. (t -> Number) -> Array t -> t
+maxBy = (f) -> (arr) ->
+  maxX = undefined
+  maxY = -Infinity
+  for x in arr
     y = f(x)
-    for item, i in arr
-      if y <= f(item)
-        return i
-    arr.length
+    if y > maxY
+      maxX = x
+      maxY = y
+  maybe(maxX)
 
-  # forall t. Array t -> Array t
-  tail: (arr) ->
-    arr.slice(1)
+# forall t. Array t -> Array t
+reverse = (xs) ->
+  xs[..].reverse()
 
-  # forall t. Array (String, t) -> Object t
-  toObject: (arr) ->
-    out = {}
-    for [a, b] in arr
-      out[a] = b
-    out
+# forall t. t -> Array t
+singleton = (x) ->
+  [x]
 
-  # forall t. Array t -> Array t
-  unique: (arr) ->
-    acc = []
-    for x in arr
-      if not arrayOps.contains(x)(acc)
-        acc.push(x)
-    acc
+# forall t. (t -> Comparable) -> Array t -> Array t
+sortBy = (f) -> (arr) ->
+  g = (x, y) ->
+    fx = f(x)
+    fy = f(y)
+    if fx < fy then -1 else if fx > fy then 1 else 0
+  arr.slice(0).sort(g)
 
-  # forall t u. (t -> u) -> Array t -> Array t
-  uniqueBy: (f) -> (arr) ->
-    acc  = []
-    seen = []
-    for x in arr
-      y = f(x)
-      if not arrayOps.contains(y)(seen)
-        seen.push(y)
-        acc.push(x)
-    acc
+# forall t. (t -> Comparable) -> Array t -> t -> Number
+# Assumes that `arr` is already sorted in terms of `f`
+sortedIndexBy = (f) -> (arr) -> (x) ->
+  y = f(x)
+  for item, i in arr
+    if y <= f(item)
+      return i
+  arr.length
 
-  # forall t u. Array t -> Array u -> Array (t, u)
-  zip: (xs) -> (arr) ->
-    out    = []
-    length = Math.min(xs.length, arr.length)
-    for i in [0...length]
-      out.push([xs[i], arr[i]])
-    out
+# forall t. Array t -> Array t
+tail = (arr) ->
+  arr.slice(1)
 
-}
+# forall t. Array (String, t) -> Object t
+toObject = (arr) ->
+  out = {}
+  for [a, b] in arr
+    out[a] = 0
+  out
 
-module.exports = arrayOps
+# forall t. Array t -> Array t
+unique = (arr) ->
+  acc = []
+  for x in arr
+    if not contains(x)(acc)
+      acc.push(x)
+  acc
+
+# forall t u. (t -> u) -> Array t -> Array t
+uniqueBy = (f) -> (arr) ->
+  acc  = []
+  seen = []
+  for x in arr
+    y = f(x)
+    if not contains(y)(seen)
+      seen.push(y)
+      acc.push(x)
+  acc
+
+# forall t u. Array t -> Array u -> Array (t, u)
+zip = (xs) -> (arr) ->
+  out    = []
+  length = Math.min(xs.length, arr.length)
+  for i in [0...length]
+    out.push([xs[i], arr[i]])
+  out
+
+export { all, concat, contains, countBy, difference, exists, filter, find, findIndex, flatMap, flattenDeep, foldl, forEach, head, isEmpty, item, last, length, map, maxBy, prepend, reverse, singleton, sortBy, sortedIndexBy, tail, toObject, unique, uniqueBy, zip }

test/array.coffee
@@ -1,6 +1,8 @@
-{ all, concat, contains, countBy, difference, exists, filter, find, findIndex, flatMap, flattenDeep, foldl, forEach, head, isEmpty, item, last, length, map, maxBy, prepend, reverse, singleton, sortBy, sortedIndexBy, tail, toObject, unique, uniqueBy, zip } = require('brazier/array')
-{ id, pipeline    } = require('brazier/function')
-{ None, Something } = require('brazier/maybe')
+import { QUnit } from '/test/target/qunit/qunit.js'
+
+import { all, concat, contains, countBy, difference, exists, filter, find, findIndex, flatMap, flattenDeep, foldl, forEach, head, isEmpty, item, last, length, map, maxBy, prepend, reverse, singleton, sortBy, sortedIndexBy, tail, toObject, unique, uniqueBy, zip } from '/target/brazier/brazier/array.js'
+import { id, pipeline    } from '/target/brazier/brazier/function.js'
+import { None, Something } from '/target/brazier/brazier/maybe.js'
 
 exploder = (x) -> throw new Error("This code should not get run.")
 

Gruntfile.coffee
@@ -44,15 +44,6 @@ module.exports = (grunt) ->
       }
 
     },
-    browserify: {
-      main: {
-        src: ['target/brazier/bootstrap.js'],
-        dest: 'dist/brazier.js',
-        options: {
-          alias: []
-        }
-      }
-    },
     copy: {
 
       publish: {
@@ -80,34 +71,57 @@ module.exports = (grunt) ->
           , flatten: true
           },
 
-          {
-            src: ['dist/brazier.js']
-          , dest: 'test/target/brazier.js'
-          }
-
         ],
       },
 
     },
+    file_append: {
+      default_options: {
+        files: [
+          {
+            append: "const QUnit = window?.QUnit; export { QUnit };",
+            input:  'test/target/qunit/qunit.js',
+            output: 'test/target/qunit/qunit.js'
+          }
+        ]
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          protocol: 'http',
+          port:     9005,
+          middleware: (connect, options, middlewares) ->
+                        middlewares.unshift((req, res, next) ->
+                          if req.url.endsWith('.js')
+                            res.setHeader('Content-Type', 'text/javascript')
+                          next()
+                        )
+                        middlewares
+        },
+      },
+    },
     qunit: {
-      all: ['test/*.html']
+      all: ['test/*.html'],
+      options: {
+        puppeteer: {
+          ignoreDefaultArgs: true,
+          args: [
+            "--headless",
+            "--disable-web-security",
+            "--allow-file-access-from-files"
+          ]
+        },
+      }
     }
   })
 
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-coffeelint');
+  grunt.loadNpmTasks('grunt-coffeelint')
   grunt.loadNpmTasks('grunt-contrib-coffee')
+  grunt.loadNpmTasks('grunt-contrib-connect')
   grunt.loadNpmTasks('grunt-contrib-copy')
   grunt.loadNpmTasks('grunt-contrib-qunit')
+  grunt.loadNpmTasks('grunt-file-append')
 
-  # I do this because inlining it in `browserify`'s options causes the value to
-  # be evaluated before any of the tasks are run, but we need to wait until
-  # `coffee` runs for this to work --JAB (8/21/14)
-  grunt.task.registerTask('gen_aliases', 'Find aliases, then run browserify', ->
-    aliases = massAlias('./target/brazier/**/*.js', 'brazier')
-    grunt.config(['browserify', 'main', 'options', 'alias'], aliases);
-    return
-  )
-
-  grunt.registerTask('default', ['coffeelint', 'coffee:compile', 'copy:publish', 'gen_aliases', 'browserify'])
-  grunt.registerTask('test',    ['default', 'coffee:test', 'copy:test', 'qunit'])
+  grunt.registerTask('default', ['coffeelint', 'coffee:compile', 'copy:publish'])
+  grunt.registerTask('test',    ['default', 'coffee:test', 'copy:test', 'file_append', 'connect', 'qunit'])

src/bootstrap.coffee
@@ -1,9 +1,9 @@
-require('brazier/array')
-require('brazier/equals')
-require('brazier/function')
-require('brazier/maybe')
-require('brazier/number')
-require('brazier/object')
-require('brazier/type')
+import 'brazier/array.js'
+import 'brazier/equals.js'
+import 'brazier/function.js'
+import 'brazier/maybe.js'
+import 'brazier/number.js'
+import 'brazier/object.js'
+import 'brazier/type.js'
 
-module.exports = ->
+export default (->)

src/brazier/equals.coffee
@@ -1,4 +1,4 @@
-{ isArray, isBoolean, isNumber, isObject, isString } = require('./type')
+import { isArray, isBoolean, isNumber, isObject, isString } from './type.js'
 
 # forall t. Array t -> Array t -> Boolean
 arrayEquals = (x) -> (y) ->
@@ -44,7 +44,7 @@ objectEquals = (x) -> (y) ->
 stringEquals = (x) -> (y) ->
   x is y
 
-module.exports = {
+export {
   arrayEquals
   booleanEquals
   eq

src/brazier/function.coffee
@@ -1,50 +1,48 @@
-module.exports = {
+# forall t u. (t -> u) -> t -> u
+apply = (f) -> (x) ->
+  f(x)
 
-  # forall t u. (t -> u) -> t -> u
-  apply: (f) -> (x) ->
-    f(x)
+# forall t. t -> Unit -> t
+constantly = (x) ->
+  -> x
 
-  # forall t. t -> Unit -> t
-  constantly: (x) ->
-    -> x
+# Function? -> (? -> ?)
+curry = (f) ->
+  argsToArray = (args) -> Array.prototype.slice.call(args, 0)
+  curryMaster = ->
+    argsThusFar = argsToArray(arguments)
+    if argsThusFar.length >= f.length
+      f(argsThusFar...)
+    else
+      ->
+        nextTierArgs = argsToArray(arguments)
+        curryMaster(argsThusFar.concat(nextTierArgs)...)
+  curryMaster
 
-  # Function? -> (? -> ?)
-  curry: (f) ->
-    argsToArray = (args) -> Array.prototype.slice.call(args, 0)
-    curryMaster = ->
-      argsThusFar = argsToArray(arguments)
-      if argsThusFar.length >= f.length
-        f(argsThusFar...)
-      else
-        ->
-          nextTierArgs = argsToArray(arguments)
-          curryMaster(argsThusFar.concat(nextTierArgs)...)
-    curryMaster
+# forall t u v. (t -> u -> v) -> (u -> t -> v)
+flip = (f) ->
+  (x) -> (y) -> f(y)(x)
 
-  # forall t u v. (t -> u -> v) -> (u -> t -> v)
-  flip: (f) ->
-    (x) -> (y) -> f(y)(x)
+# forall t. t -> t
+id = (x) ->
+  x
 
-  # forall t. t -> t
-  id: (x) ->
-    x
+# forall t. (t -> t)* -> (t -> t)
+pipeline = (functions...) ->
+  (args...) ->
+    [h, fs...] = functions
+    out = h(args...)
+    for f in fs
+      out = f(out)
+    out
 
-  # forall t. (t -> t)* -> (t -> t)
-  pipeline: (functions...) ->
-    (args...) ->
-      [h, fs...] = functions
-      out = h(args...)
-      for f in fs
-        out = f(out)
-      out
+# forall t u v. (t -> u) -> (t -> v) -> t -> (u, v)
+tee = (f) -> (g) ->
+  (x) -> [f(x), g(x)]
 
-  # forall t u v. (t -> u) -> (t -> v) -> t -> (u, v)
-  tee: (f) -> (g) ->
-    (x) -> [f(x), g(x)]
+# (? -> ?) -> Function?
+uncurry = (f) ->
+  (args...) ->
+    args.reduce(((acc, arg) -> acc(arg)), f)
 
-  # (? -> ?) -> Function?
-  uncurry: (f) ->
-    (args...) ->
-      args.reduce(((acc, arg) -> acc(arg)), f)
-
-}
+export { apply, constantly, curry, flip, id, pipeline, tee, uncurry }

src/brazier/maybe.coffee
@@ -1,40 +1,36 @@
-maybeOps = {
+# Maybe Nothing
+None = {}
 
-  # Maybe Nothing
-  None: {}
+# forall t. t -> Maybe t
+Something = (x) ->
+  { _type: "something", _value: x }
 
-  # forall t. t -> Maybe t
-  Something: (x) ->
-    { _type: "something", _value: x }
+# forall t. (t -> Boolean) -> Maybe t -> Maybe t
+filter = (f) -> (maybe) ->
+  flatMap((x) -> if f(x) then Something(x) else None)(maybe)
 
-  # forall t. (t -> Boolean) -> Maybe t -> Maybe t
-  filter: (f) -> (maybe) ->
-    maybeOps.flatMap((x) -> if f(x) then maybeOps.Something(x) else maybeOps.None)(maybe)
+# forall t u. (t -> Maybe u) -> Maybe t -> Maybe u
+flatMap = (f) -> (maybe) ->
+  fold(-> None)(f)(maybe)
 
-  # forall t u. (t -> Maybe u) -> Maybe t -> Maybe u
-  flatMap: (f) -> (maybe) ->
-    maybeOps.fold(-> maybeOps.None)(f)(maybe)
+# forall t u. (Unit -> u) -> (t -> u) -> Maybe t -> u
+fold = (ifNone) -> (ifSomething) -> (maybe) ->
+  if isSomething(maybe) then ifSomething(maybe._value) else ifNone()
 
-  # forall t u. (Unit -> u) -> (t -> u) -> Maybe t -> u
-  fold: (ifNone) -> (ifSomething) -> (maybe) ->
-    if maybeOps.isSomething(maybe) then ifSomething(maybe._value) else ifNone()
+# Maybe _ -> Boolean
+isSomething = ({ _type }) ->
+  _type is "something"
 
-  # Maybe _ -> Boolean
-  isSomething: ({ _type }) ->
-    _type is "something"
+# forall t u. (t -> u) -> Maybe t -> Maybe u
+map = (f) -> (maybe) ->
+  fold(-> None)((x) -> Something(f(x)))(maybe)
 
-  # forall t u. (t -> u) -> Maybe t -> Maybe u
-  map: (f) -> (maybe) ->
-    maybeOps.fold(-> maybeOps.None)((x) -> maybeOps.Something(f(x)))(maybe)
+# forall t. t -> Maybe t
+maybe = (x) ->
+  if x? then Something(x) else None
 
-  # forall t. t -> Maybe t
-  maybe: (x) ->
-    if x? then maybeOps.Something(x) else maybeOps.None
+# forall t. Maybe t -> Array t
+toArray = (maybe) ->
+  fold(-> [])((x) -> [x])(maybe)
 
-  # forall t. Maybe t -> Array t
-  toArray: (maybe) ->
-    maybeOps.fold(-> [])((x) -> [x])(maybe)
-
-}
-
-module.exports = maybeOps
+export { filter, flatMap, fold, isSomething, map, maybe, None, Something, toArray }

src/brazier/number.coffee
@@ -1,25 +1,23 @@
-module.exports = {
+# Number -> Number -> Number
+multiply = (x) -> (y) ->
+  x * y
 
-  # Number -> Number -> Number
-  multiply: (x) -> (y) ->
-    x * y
+# Number -> Number -> Number
+plus = (x) -> (y) ->
+  x + y
 
-  # Number -> Number -> Number
-  plus: (x) -> (y) ->
-    x + y
+# Number -> Number -> Array Number
+rangeTo = (start) -> (end) ->
+  if start <= end
+    [start..end]
+  else
+    []
 
-  # Number -> Number -> Array Number
-  rangeTo: (start) -> (end) ->
-    if start <= end
-      [start..end]
-    else
-      []
+# Number -> Number -> Array Number
+rangeUntil = (start) -> (end) ->
+  if start < end
+    [start...end]
+  else
+    []
 
-  # Number -> Number -> Array Number
-  rangeUntil: (start) -> (end) ->
-    if start < end
-      [start...end]
-    else
-      []
-
-}
+export { multiply, plus, rangeTo, rangeUntil }

commit 8950a07daa6ead8438e7ed4f9847e5b43494e219

Bugfix: Unbork `Array.toObject`
Somehow, this bug was introduced in da8de529a38143286f79fa83f17aa52d601c570f / v3.0.0, when switching from Browserify.  I have no explanation for how.

src/brazier/array.coffee
@@ -163,7 +163,7 @@ tail = (arr) ->
 toObject = (arr) ->
   out = {}
   for [a, b] in arr
-    out[a] = 0
+    out[a] = b
   out
 
 # forall t. Array t -> Array t